c134c9832e7eecb846cbc99bf523f2ab4ecc897a
fjb56 <fjb56@uclive.ac.nz>
2023-05-10 22:05:59 +1200

    Upgrading dependencies, currently stable

--- app/src/main/java/com/example/japanese_reps/sqliteHelper/DBHelper.kt
+++ app/src/main/java/com/example/japanese_reps/sqliteHelper/DBHelper.kt
@@ -1,4 +1,38 @@
 package com.example.japanese_reps.sqliteHelper
 
-class DBHelper {
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.Statement
+
+fun createAndPopulateDatabase() {
+    // Step 1: Connect to the database
+    val connection: Connection = DriverManager.getConnection("jdbc:sqlite:mydatabase.db")
+
+    // Step 2: Create the table
+    val statement: Statement = connection.createStatement()
+    val createTableQuery = """
+        CREATE TABLE IF NOT EXISTS mytable (
+            id INTEGER PRIMARY KEY,
+            english TEXT,
+            romanji TEXT,
+            hiragana TEXT,
+            katakana TEXT,
+            kanji TEXT,
+            most_common_form TEXT
+        )
+    """
+    statement.execute(createTableQuery)
+
+    // Step 3: Insert data into the table
+    val insertDataQuery = """
+        INSERT INTO mytable (id, english, romanji, hiragana, katakana, kanji, most_common_form)
+        VALUES
+            (1, 'Hello', 'Konnichiwa', 'こんにちは', 'コンニチハ', '今日は', 'Konnichiwa'),
+            (2, 'Goodbye', 'Sayonara', 'さようなら', 'サヨウナラ', 'さようなら', 'Sayonara')
+    """
+    statement.execute(insertDataQuery)
+
+    // Step 4: Close the database connection
+    statement.close()
+    connection.close()
 }
